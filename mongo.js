993ed9d4f446d37cebd2c6aad4715b782f04ccf8
gitMykola <mykola_borodyn@ecoengineer.in.ua>
2017-11-21 21:19:42 +0100

    Mok logic add

--- mongo.js
+++ mongo.js
@@ -1,6 +1,12 @@
 const config = require('./config.json');
 const url = config.db.url;
 const db = require('monk')(url);
+const collection = db.get('ether_transactions');
+
 db.then(()=>{
     console.log('Connected correctly to server');
+    collection.count({},function(error, count){
+	console.log(count);
+    });
+    //console.log(collection.count({}));
 });
