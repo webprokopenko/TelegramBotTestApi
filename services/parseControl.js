e8aa49c5fcb3786a214fbd7d9baa533e2ddd47b4
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-11-21 22:33:23 +0200

    Prepare Parse Control

---
+++ services/parseControl.js
@@ -0,0 +1,22 @@
+const config = require('./config.json');
+const url = config.db.url;
+const db = require('monk')(url);
+const collection = db.get('ether_transactions');
+
+function getLastParse(){
+        db.then(()=>{
+            collection.count({},function(error, count){
+            return count;
+            });
+        });
+};
+function parseControl(countDoc){
+        db.then(()=>{
+            collection.count({},function(error,count){
+                if(countDoc<=count)
+                    return false;
+                else
+                    return true;
+            });
+        });
+}
